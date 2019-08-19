3d807cff56b5f58fa304b6dfc002b2e87d6f871a
Alik-A <33788103+Alik-A@users.noreply.github.com>
2019-08-19 14:44:21 +0300

    fix bugs. added option to include upper case letters in password

--- passGeneratorJS.js
+++ passGeneratorJS.js
@@ -14,45 +14,55 @@ function onLoad() {
 			output.style.backgroundColor = "black";
 		}
 	};
-	document.getElementById("autoCopyChkBox").checked = localStorage.autoCopy;
 }
 
 function generateNewPass() {
-	var passComponentsArr = new Array(1, 2, 3);
-	if(!document.getElementById("includeLettersChkBox").checked) delete passComponentsArr[0];
-	if(!document.getElementById("includeNumsChkBox").checked) delete passComponentsArr[1];
-	if(!document.getElementById("includeSpCharsChkBox").checked) delete passComponentsArr[2];
+	var passComponentsArr = new Array();
+	if(document.getElementById("includeLowCaseLettersChkBox").checked) passComponentsArr.push(1);
+	if(document.getElementById("includeUpCaseLettersChkBox").checked) passComponentsArr.push(2);
+	if(document.getElementById("includeSpCharsChkBox").checked) passComponentsArr.push(3);
+	if(document.getElementById("includeNumsChkBox").checked) passComponentsArr.push(4);
 	
 	var generatedPass = new String();
 	var typeForGenerate = 0;
 	var passLength = Number(document.getElementById("passLenCB").value);
+	console.log(passComponentsArr.length);
+	if(passComponentsArr.length == 0) {
+		alert("choose at least one type of characters to generate password");
+		return;
+	}
 	while(passLength-- != 0) {
 		do {
-			typeForGenerate = Math.floor(Math.random() * (3 - 1) + 1);
-			console.log(typeForGenerate);
-		}
+			typeForGenerate = Math.floor(Math.random() * 4 + 1);
+ 		}
 		while(!passComponentsArr.includes(typeForGenerate));
-			console.log(generatedPass);
 			switch(typeForGenerate) {
 				case 1: 
-					generatedPass += String.fromCharCode(generateLetterAscii());
+					generatedPass += String.fromCharCode(generateLowCaseLetterAscii());
 					break;
 					
 				case 2: 
-					generatedPass += String.fromCharCode(generateNum());
+					generatedPass += String.fromCharCode(generateUpCaseLetterAscii());
 					break;
 				
 				case 3: 
 					generatedPass += String.fromCharCode(generateSpCharAscii());
 					break;
+					
+				case 4: 
+					generatedPass += String.fromCharCode(generateNum());
 			};
 	}
-		output.value = generatedPass;
-
+	output.value = generatedPass;
+	document.getElementById("copyToClipBrdBtn").disabled = false;
 }
 
-function generateLetterAscii() {
+function generateLowCaseLetterAscii(includeCapLetters) {
 	return(Math.floor(Math.random() * (122 - 97)) + 97);
+}
+
+function generateUpCaseLetterAscii() {
+	return(Math.floor(Math.random() * (90 - 65)) + 65);
 }
 
 function generateNum() {
@@ -60,12 +70,23 @@ function generateNum() {
 }
 
 function generateSpCharAscii() {
-	switch(Math.ceil(Math.random() * 2)) {
+	switch(Math.floor(Math.random() * (3 - 1) + 1)) {
 		case 1: 
-			return(Math.floor(Math.random() *  (47 - 33)) + 47);
+			return(Math.floor(Math.random() *  (47 - 33)) + 33);
 		case 2: 
 			return(Math.floor(Math.random() *  (64 - 58)) + 58);
 		case 3: 
 			return(Math.floor(Math.random() *  (96 - 91)) + 91);
 	};
+}
+
+function copyGeneratedToClipBrd() {
+	document.getElementById("passOutput").select(0, 1);
+	document.execCommand("copy");
+	output.style.background = "#00ff00";
+	output.style.color = "#000000";
+	document.getElementById("passOutput").selectionEnd = 0;
+
+	output.blur();
+
 }
